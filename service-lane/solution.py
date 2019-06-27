ef7ed3eb0c67b2e3b9193758352138642b796dbe
mihir <phatak.mihir@gmail.com>
2019-06-27 16:16:22 -0400

    Service lane c++ and python

---
+++ service-lane/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the serviceLane function below.
+def serviceLane(n, cases):
+    mins = []
+    for iter1,ele1 in enumerate(cases):
+        mins.append(min(width[ele1[0]:ele1[1]+1]))
+
+    return mins
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nt = input().split()
+
+    n = int(nt[0])
+
+    t = int(nt[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
